b84e9f6597320617ab2ccf76fef959c3afdc010a
Shivesh Ranjan <shivesh.ranjan@gmail.com>
2020-08-06 08:38:59 -0700

    making the code async
    Signed-off-by: Shivesh Ranjan <shivesh.ranjan@gmail.com>

--- test/integration/src/main/java/io/pravega/test/integration/demo/Sample.java
+++ test/integration/src/main/java/io/pravega/test/integration/demo/Sample.java
@@ -67,73 +67,78 @@ public class Sample {
         rgm.createReaderGroup(rgName, cfg);
         ReaderGroup rg = rgm.getReaderGroup(rgName);
 
-        periodicRolloverAndTruncation(controller, scope, stream, streamObj, Collections.singletonList(rg), run);
-        processEvents(clientFactory, rgName, run);
+        startBackgroundRolloverAndTruncation(controller, scope, stream, streamObj, Collections.singletonList(rg), run);
+        backgroundProcessEvents(clientFactory, rgName, run);
 
         Thread.sleep(10000000L);
         run.set(false);
     }
 
-    private static void periodicRolloverAndTruncation(ControllerImpl controller, String scope, String stream, StreamImpl streamObj,
-                                                      List<ReaderGroup> rg, AtomicBoolean run) throws InterruptedException {
+    private static void startBackgroundRolloverAndTruncation(ControllerImpl controller, String scope, String stream, StreamImpl streamObj,
+                                                             List<ReaderGroup> rg, AtomicBoolean run) {
         AtomicReference<StreamCut> truncationPoint = new AtomicReference<>();
-        truncationPoint.set(rolloverAndGetTruncationPoint(controller, scope, stream, streamObj));
-
-        Futures.loop(run::get, () -> Futures.delayedFuture(() -> {
-            return Futures.allOfWithResults(rg.stream().map(m -> m.generateStreamCuts(EXECUTOR).thenApply(streamCut -> {
-                return streamCut.get(streamObj).asImpl()
-                                .getPositions().entrySet().stream().allMatch(x -> {
-                            Map<Segment, Long> positions = truncationPoint.get().asImpl().getPositions();
-                            if (positions.containsKey(x.getKey())) {
-                                return positions.get(x.getKey()) > x.getValue();
-                            } else {
-                                return positions.entrySet().stream().allMatch(y -> x.getKey().getSegmentId() > y.getKey().getSegmentId());
-                            }
-                        });
-            })).collect(Collectors.toList()))
-                          .thenCompose(list -> {
-                              boolean allAhead = list.stream().reduce(true, (a, b) -> a && b);
-
-                              if (allAhead) {
-                                  return controller.truncateStream(scope, stream, truncationPoint.get())
-                                                   .thenAccept(v -> truncationPoint.set(rolloverAndGetTruncationPoint(controller, scope, stream, streamObj)));
-                              } else {
-                                  return CompletableFuture.<Void>completedFuture(null);
-                              }
-                          });
-        }, Duration.ofSeconds(10).toMillis(), EXECUTOR), EXECUTOR);
+        rolloverAndGetTruncationPoint(controller, scope, stream, streamObj)
+                .thenCompose(tp -> {
+                    truncationPoint.set(tp);
+                    return Futures.loop(run::get, () -> Futures.delayedFuture(() -> {
+                        return Futures.allOfWithResults(rg.stream().map(m -> m.generateStreamCuts(EXECUTOR).thenApply(streamCut -> {
+                            return streamCut.get(streamObj).asImpl()
+                                            .getPositions().entrySet().stream().allMatch(x -> {
+                                        Map<Segment, Long> positions = truncationPoint.get().asImpl().getPositions();
+                                        if (positions.containsKey(x.getKey())) {
+                                            return positions.get(x.getKey()) > x.getValue();
+                                        } else {
+                                            return positions.entrySet().stream().allMatch(y -> x.getKey().getSegmentId() > y.getKey().getSegmentId());
+                                        }
+                                    });
+                        })).collect(Collectors.toList()))
+                                      .thenCompose(list -> {
+                                          boolean allAhead = list.stream().reduce(true, (a, b) -> a && b);
+                                          if (allAhead) {
+                                              return controller.truncateStream(scope, stream, truncationPoint.get())
+                                                               .thenCompose(v -> rolloverAndGetTruncationPoint(controller, scope, stream, streamObj))
+                                                               .thenAccept(truncationPoint::set);
+                                          } else {
+                                              return CompletableFuture.<Void>completedFuture(null);
+                                          }
+                                      });
+                    }, Duration.ofSeconds(10).toMillis(), EXECUTOR), EXECUTOR);
+                });
     }
 
-    private static StreamCut rolloverAndGetTruncationPoint(ControllerImpl controller, String scope, String stream, StreamImpl streamObj) {
+    private static CompletableFuture<StreamCut> rolloverAndGetTruncationPoint(ControllerImpl controller, String scope, String stream, StreamImpl streamObj) {
         // this is a new method that i had to add to controller client because it returned an opaque StreamSegments object from 
         // getCurrentSegments which did not expose the segments with their ranges, which is required to create identical 
         // replacement ranges.
-        List<SegmentWithRange> activeSegments = controller.getCurrentSegmentsWithRange(scope, stream).join();
+        return controller.getCurrentSegmentsWithRange(scope, stream)
+                  .thenCompose(activeSegments -> {
+                      List<Long> segmentsToSeal = new ArrayList<>();
+                      Map<Double, Double> newRanges = new HashMap<>();
+                      activeSegments.forEach(x -> {
+                          segmentsToSeal.add(x.getSegment().getSegmentId());
+                          newRanges.put(x.getRange().getLow(), x.getRange().getHigh());
+                      });
 
-        List<Long> segmentsToSeal = new ArrayList<>();
-        Map<Double, Double> newRanges = new HashMap<>();
-        activeSegments.forEach(x -> {
-            segmentsToSeal.add(x.getSegment().getSegmentId());
-            newRanges.put(x.getRange().getLow(), x.getRange().getHigh());
-        });
-
-        controller.scaleStream(streamObj, segmentsToSeal, newRanges, EXECUTOR).getFuture().join();
-
-        // get the segments post scale and create a stream cut from them.
-        activeSegments = controller.getCurrentSegmentsWithRange(scope, stream).join();
-        Map<Segment, Long> map = activeSegments.stream().collect(Collectors.toMap(SegmentWithRange::getSegment, x -> 0L));
-
-        return new StreamCutImpl(streamObj, map);
+                      // this could fail if active segments were sealed by the time we attempted to scale them. 
+                      // TODO: it should be retried for scale precondition failures. 
+                      return controller.scaleStream(streamObj, segmentsToSeal, newRanges, EXECUTOR).getFuture()
+                              .thenApply(v -> {
+                                  // get the segments post scale and create a stream cut from them.
+                                  List<SegmentWithRange> newSegments = controller.getCurrentSegmentsWithRange(scope, stream).join();
+                                  Map<Segment, Long> map = newSegments.stream().collect(Collectors.toMap(SegmentWithRange::getSegment, x -> 0L));
+                                  return new StreamCutImpl(streamObj, map);                                  
+                              });
+                  });
     }
 
-    private static void processEvents(EventStreamClientFactory clientFactory, String rgName, AtomicBoolean continueReading) {
+    private static void backgroundProcessEvents(EventStreamClientFactory clientFactory, String rgName, AtomicBoolean continueReading) {
         EventStreamReader<String> reader = clientFactory.createReader("reader", rgName, new JavaSerializer<>(),
                 ReaderConfig.builder().build());
 
         Futures.loop(continueReading::get, () -> {
             return CompletableFuture.runAsync(() -> {
                 EventRead<String> event = reader.readNextEvent(1000L);
-                // process event
+                // TODO: process event
                 // ...
             }, EXECUTOR);
         }, EXECUTOR);
